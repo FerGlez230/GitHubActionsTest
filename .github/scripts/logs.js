f271dd7afa95ac57b4d96f0bb90663f6cfed67ff
FerGlez230 <ferglez230@gmail.com>
2023-06-03 21:34:43 -0600

    feat: add new yml file

--- .github/scripts/logs.js
+++ .github/scripts/logs.js
@@ -1,5 +1,9 @@
 const { Octokit } = require('@octokit/rest');
 
+const octokit = new Octokit({ 
+  auth: process.env.GH_TOKEN,
+});
+
 const owner = 'FerGlez230';
 const repo = 'GitHubActionsTest';
 const branch = 'main'; // Replace with your GitHub Pages branch name
@@ -7,9 +11,7 @@ const branch = 'main'; // Replace with your GitHub Pages branch name
 const content = 'Hello, GitHub Pages!'; // The content to be written to the file
 const path = 'logs.txt'; // The path and name of the file to be created
 
-const octokit = new Octokit();
-
-octokit.repos.createOrUpdateFileContents({
+/*octokit.repos.createOrUpdateFileContents({
   owner,
   repo,
   path,
@@ -23,4 +25,38 @@ octokit.repos.createOrUpdateFileContents({
   })
   .catch(error => {
     console.error('Error:', error);
-  });
+  });*/
+
+octokit.request("GET /repos/{owner}/{repo}/issues", {
+  owner,
+  repo,
+  per_page: 2
+})
+.then((response) => {
+  console.log('hi', process.env.GH_TOKEN)
+  // console.log('File created successfully!', response);
+})
+.catch(error => {
+  console.error('Error:', error);
+})
+
+octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+  owner,
+  repo,
+  path,
+  message: 'my commit message',
+  committer: {
+    name: owner,
+    email: 'ferglez@gmail.com'
+  },
+  content: 'bXkgbmV3IGZpbGUgY29udGVudHM=',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+.then((response) => {
+  console.log('File created successfully!', response);
+})
+.catch(error => {
+  console.error('Error:', error);
+})
