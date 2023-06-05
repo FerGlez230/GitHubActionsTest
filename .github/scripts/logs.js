6e710d8a3961869f46c3a863c2f50aabbef7edc1
FerGlez230 <ferglez230@gmail.com>
2023-06-04 21:01:07 -0600

    fix: change script

--- .github/scripts/logs.js
+++ .github/scripts/logs.js
@@ -8,27 +8,63 @@ const owner = 'FerGlez230';
 const repo = 'GitHubActionsTest';
 const branch = 'main'; // Replace with your GitHub Pages branch name
 
-let content = 'Hello, GitHub Pages!'; // The content to be written to the file
+let content = {}; // The content to be written to the file
 const dateISOString = new Date().toISOString();
 const path = dateISOString.slice(0, dateISOString.indexOf('T'))+'.txt'; // The path and name of the file to be created
 
-/*octokit.repos.createOrUpdateFileContents({
-  owner,
-  repo,
-  path,
-  message: 'Create file via API',
-  content: Buffer.from(content).toString('base64'),
-  branch,
-  token: process.env.GITHUB_TOKEN, // Use the GITHUB_TOKEN provided by GitHub Actions
-})
-  .then(() => {
-    console.log('File created successfully!');
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });*/
+const currentDate = new Date().toISOString().split("T")[0];
+const sprintBegin = new Date();
+sprintBegin.setDate(sprintBegin.getDate() - 10);
+const sprintBeginDate = sprintBegin.toISOString().split("T")[0];
 
-octokit.request("GET /repos/{owner}/{repo}/issues", {
+const query = `repo:${owner}/${repo} is:issue is:closed closed:${sprintBeginDate}..${currentDate}`;
+
+
+const closedIssues = [];
+octokit.request('GET /search/issues', {
+  q: query,
+})
+  .then((response) => {
+    const issues = response.data.items;
+    console.log("Closed issues in the last 10 days:");
+    issues.forEach((issue) => {
+      closedIssues.push(issue.title)
+      console.log(`- ${issue.title}`);
+    });
+    const responseContent = {
+      numberOfClosedIssues: issues.length,
+      closedIssues
+    };
+    const jsonContent = JSON.stringify(responseContent);
+    // content = Buffer.from('jsonContent').toString('base64');
+    content = btoa(jsonContent);
+    console.log(content);
+    octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+      owner,
+      repo,
+      path,
+      message: 'Get data from API',
+      committer: {
+        name: owner,
+        email: 'fergle230@gmail.com'
+      },
+      content,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+    .then((response) => {
+      console.log('File created successfully!', response);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    })
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+/*octokit.request("GET /repos/{owner}/{repo}/issues", {
   owner,
   repo,
   per_page: 2
@@ -62,6 +98,6 @@ octokit.request("GET /repos/{owner}/{repo}/issues", {
 })
 .catch(error => {
   console.error('Error:', error);
-})
+})*/
 
 
